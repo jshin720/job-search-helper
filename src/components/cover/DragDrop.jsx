87619223083f031ee581247ffec6340af7ea7b1f
jshin720 <jeffshin720@gmail.com>
2023-08-14 16:11:19 -0400

    working on dragdrop component

--- src/components/cover/DragDrop.jsx
+++ src/components/cover/DragDrop.jsx
@@ -2,11 +2,25 @@ import { useState } from 'react'
 import './dragDrop.css'
 
 export default function DragDrop() {
-  const [files, setFiles] = useState(null);
+  const [dragActive, setDragActive] = useState(false);
+
+
+  // function handle the drag - if the use is dragging something into the component.
+  //drag event listener
+  const handleDrag = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    if (e.type === "dragenter" || e.type === "dragover") {
+      setDragActive(true);
+    } else if (e.type === "dragleave") {
+      setDragActive(false);
+    }
+  }
 
 
   return (
-    <form id="form-file-upload">
+    <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
       <input type="file" id="input-file-upload" multiple={false} />
       <label id="label-file-upload" htmlFor="input-file-upload">
         <div>
