f5849d4efd27bfad37fc6f343bb73685bd2972af
jshin720 <jeffshin720@gmail.com>
2023-08-14 16:41:02 -0400

    working on handlechange func in the dragdrop component

--- src/components/cover/DragDrop.jsx
+++ src/components/cover/DragDrop.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './dragDrop.css'
 
 export default function DragDrop() {
   const [dragActive, setDragActive] = useState(false);
-
+  const inputRef = useRef(null);
 
   // function handle the drag - if the use is dragging something into the component.
   //drag event listener
@@ -18,16 +18,56 @@ export default function DragDrop() {
     }
   }
 
+  const handleChange = (e) => {
+    e.preventDefault();
+    if (e.target.files && e.target.files[0]){
+      
+    }
+
+  }
+
+  const handleDrop = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setDragActive(false);
+
+    if (e.dataTransfer.files && e.dataTransfer.files[0]) {
+
+    }
+  }
+
+  const onButtonClick = () => {
+    inputRef.current.click()
+  }
 
   return (
-    <form id="form-file-upload" onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-      <input type="file" id="input-file-upload" multiple={false} />
+    <form
+      id="form-file-upload"
+      onDragEnter={handleDrag}
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <input
+        ref={inputRef}
+        type="file"
+        id="input-file-upload"
+        multiple={true}
+        onChange={handleChange}
+      />
       <label id="label-file-upload" htmlFor="input-file-upload">
         <div>
           <p>Drag and drop your file here or</p>
-          <button className="upload-button">Upload a file</button>
+          <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
         </div>
       </label>
+      {dragActive && (
+        <div
+          id="drag-file-element"
+          onDragEnter={handleDrag}
+          onDragLeave={handleDrag}
+          onDragOver={handleDrag}
+          onDrop={handleDrop}
+        ></div>
+      )}
     </form>
   );
 }
