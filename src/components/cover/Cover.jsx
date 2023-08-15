f54391e87477ffd5dbc418154e541e94076451c2
jshin720 <jeffshin720@gmail.com>
2023-08-15 15:30:54 -0400

    working the cover component

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -1,22 +1,37 @@
-import React, { useState, useRef } from 'react';
-import DragDrop from './DragDrop';
+import React, { useState, useRef } from "react";
+import DragDrop from "./DragDrop";
 
 function Cover() {
   const [jobPosition, setJobPosition] = useState("");
-  const [jobCompany, setJobCompany] = useState("")
+  const [jobCompany, setJobCompany] = useState("");
 
   const inputRef = useRef(null);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  }
 
 
   return (
     <div>
       <DragDrop inputRef={inputRef} />
-      <form>
-        <label>Position</label>
-        <input type="text" />
-        <label>Company</label>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="job-position">Position</label>
+        <input
+          id="job-position"
+          type="text"
+          value={jobPosition}
+          onChange={(e) => setJobPosition(e.target.value)}
+        />
+        <label for="company-name">Company</label>
+        <input
+          id="company-name"
+          type="text"
+          value={jobCompany}
+          onChange={(e) => setJobCompany(e.target.value)}
+        />
+        <input type="submit" value="value"/>
       </form>
     </div>
   );
