df5f174f4688dd4ce1037980aeee0ce3f8bc7de7
jshin720 <jeffshin720@gmail.com>
2023-08-21 14:32:57 -0400

    combining cover and dragdrop component

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -50,8 +50,30 @@ function Cover() {
         onDragEnter={handleDrag}
         onSubmit={(e) => e.preventDefault()}
       >
-
-        
+        <input
+          ref={inputRef}
+          type="file"
+          id="input-file-upload"
+          multiple={false}
+          onChange={handleChange}
+        />
+        <label id="label-file-upload" htmlFor="input-file-upload">
+          <div>
+            <p>Drag and drop your file here or</p>
+            <button className="upload-button" onClick={onButtonClick}>
+              Upload a file
+            </button>
+          </div>
+        </label>
+        {dragActive && (
+          <div
+            id="drag-file-element"
+            onDragEnter={handleDrag}
+            onDragLeave={handleDrag}
+            onDragOver={handleDrag}
+            onDrop={handleDrop}
+          ></div>
+        )}
       </form>
 
       <form onSubmit={handleSubmit}>
--- src/components/cover/DragDrop.jsx
+++ src/components/cover/DragDrop.jsx
@@ -16,28 +16,6 @@ export default function DragDrop(props) {
 
   return (
     
-      <input
-        ref={inputRef}
-        type="file"
-        id="input-file-upload"
-        multiple={false}
-        onChange={handleChange}
-      />
-      <label id="label-file-upload" htmlFor="input-file-upload">
-        <div>
-          <p>Drag and drop your file here or</p>
-          <button className="upload-button" onClick={onButtonClick}>Upload a file</button>
-        </div>
-      </label>
-      {dragActive && (
-        <div
-          id="drag-file-element"
-          onDragEnter={handleDrag}
-          onDragLeave={handleDrag}
-          onDragOver={handleDrag}
-          onDrop={handleDrop}
-        ></div>
-      )}
-    
+      
   );
 }
