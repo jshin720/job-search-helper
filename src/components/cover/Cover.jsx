a1328ee2b6591ae6db111b97a3905737dc5d82a8
jshin720 <jeffshin720@gmail.com>
2023-08-22 12:56:37 -0400

    combining cover and dragdrop component

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from "react";
-import DragDrop from "./DragDrop";
+// import DragDrop from "./DragDrop";
 
 function Cover() {
   const [jobPosition, setJobPosition] = useState("");
--- src/components/cover/DragDrop.jsx
+++ src/components/cover/DragDrop.jsx
@@ -2,7 +2,6 @@ import { useState, useRef } from 'react'
 import './dragDrop.css'
 
 export default function DragDrop(props) {
-  const [dragActive, setDragActive] = useState(false);
   const { inputRef } = props;
 
   // function handle the drag - if the use is dragging something into the component.
