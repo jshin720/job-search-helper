cc3b84df2d0dafdd514d8a3a66c90353a6f4d409
jshin720 <jeffshin720@gmail.com>
2023-08-23 17:30:43 -0400

    combining cover and dragdrop component

--- src/components/cover/Cover.jsx
+++ src/components/cover/Cover.jsx
@@ -12,12 +12,12 @@ function Cover() {
      e.preventDefault();
      e.stopPropagation();
 
-    //  if (e.type === "dragenter" || e.type === "dragover") {
-    //    setDragActive(true);
-    //  } else if (e.type === "dragleave") {
-    //    setDragActive(false);
-    //  }
-   };
+  //   //  if (e.type === "dragenter" || e.type === "dragover") {
+  //   //    setDragActive(true);
+  //   //  } else if (e.type === "dragleave") {
+  //   //    setDragActive(false);
+  //   //  }
+  //  };
      const handleDrop = (e) => {
        console.log("handleDrop", e);
        e.preventDefault();
